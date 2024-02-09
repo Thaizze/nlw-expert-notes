e9385bdaefd03170c2fd2f09855b1dc7c12931d1
Thaizze Utida <thaizze.utida@gmail.com>
2024-02-09 12:40:29 -0300

    Desenvolvimento do layout da home

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,23 @@
+import logo from './assets/logo.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+      <img src={logo} alt="Logo NLW Expert" />
+
+      <form className='w-full'>
+        <input type='text' className='w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none' placeholder='Busque ...' />
+      </form>
+
+      <div className='h-px bg-slate-700' />
+
+      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+        <NewNoteCard />
+        <NoteCard />
+      </div>
+    </div>
   )
 }
 
