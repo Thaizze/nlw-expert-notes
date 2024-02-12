fc242af08960a9e2679feb435cf59c1155257f8d
Thaizze Utida <thaizze.utida@gmail.com>
2024-02-12 10:13:18 -0300

    Ajustes finais no estilo do modal

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -4,7 +4,7 @@ import { X } from 'lucide-react'
 export function NewNoteCard() {
     return(
         <Dialog.Root>
-            <Dialog.Trigger className='rounded-md flex flex-col bg-slate-700 p-5 gap-3 overflow-hidden text-left'>
+            <Dialog.Trigger className='rounded-md flex flex-col bg-slate-700 p-5 gap-3 outline-none overflow-hidden text-left hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
             <span className='text-sm font-medium text-slate-200'>Adicionar Nota</span>
             <p className='text-sm leading-6 text-slate-400'>Grave uma nota em áudio que será convertida para texto automaticamente</p>
             </Dialog.Trigger>
@@ -18,10 +18,10 @@ export function NewNoteCard() {
 
                     <div className='flex flex-1 flex-col gap-3 p-5'>
                         <span className='text-sm font-medium text-slate-300'>Adicionar Nota</span>
-                        <p className='text-sm leading-6 text-slate-400'>Comece gravando uma nota em áudio, ou se preferir utilize apenas um texto!</p>
+                        <p className='text-sm leading-6 text-slate-400'>Comece <button className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio, ou se preferir <button className='font-medium text-lime-400 hover:underline'>utilize apenas um texto</button>.</p>
                     </div>
 
-                    <button type='button' className='w-full bg-slate-800 py-4 text-sm text-slate-300 outline-none font-medium group'>Deseja <span className='text-red-400 group-hover:underline'>apagar essa nota</span>?</button>                
+                    <button type='button' className='w-full bg-lime-400 py-4 text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'>Salvar Nota</button>                
                 </Dialog.Content>
             </Dialog.Portal>
         </Dialog.Root>
