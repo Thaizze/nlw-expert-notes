a869de26e12a9fd02d249c235d08e0aca2c625a0
Thaizze Utida <thaizze.utida@gmail.com>
2024-02-15 13:21:22 -0300

    Integração com o Speech Recognition - Finalização da aplicação

--- src/app.tsx
+++ src/app.tsx
@@ -33,6 +33,15 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function onNoteDeleted(id: string){    
+    const notesArray = notes.filter(note => {
+      return note.id !== id  
+    })
+
+    setNotes(notesArray)
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
   function handleSearch(event: ChangeEvent<HTMLInputElement>){
     const query = event.target.value
 
@@ -42,7 +51,7 @@ export function App() {
   const filterNotes = search !== '' ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : notes
   
   return (
-    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+    <div className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
       <img src={logo} alt="Logo NLW Expert" />
 
       <form className='w-full'>
@@ -56,10 +65,10 @@ export function App() {
 
       <div className='h-px bg-slate-700' />
 
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+      <div className='grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard onNoteCreated={onNoteCreated} />
         {filterNotes.map(note =>{
-          return <NoteCard key={note.id} note={note} />
+          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
         })}
       </div>
     </div>
